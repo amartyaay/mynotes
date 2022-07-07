4ddb5c39c325f353c77163010d52511a9f13aa90
amartya <62711821+amartyaay@users.noreply.github.com>
2022-07-07 19:20:44 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/views/login_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginView(),
+    ),
+  );
+}
