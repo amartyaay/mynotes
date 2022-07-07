ae6dc5ae75a408c4f25faa6f56b59b714d04bb2a
amartya <62711821+amartyaay@users.noreply.github.com>
2022-07-07 19:41:43 +0530

    seperated firebase intialisation from login and register

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 
 void main() {
@@ -9,7 +12,40 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ),
   );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Colors.teal,
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              final bool emailVerified = user?.emailVerified ?? false;
+              if (emailVerified) {
+                return const Text('email is verfied');
+              } else {
+                return const Text('pls verify ur email add');
+              }
+            default:
+              return const Text('Loading');
+          }
+        },
+      ),
+    );
+  }
 }
